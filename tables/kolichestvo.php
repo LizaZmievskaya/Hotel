c0d0f57bf0817c5e96c0dd3aebcc191a8e62a431
LizaZmievskaya <zmievskaya.liza@mail.ru>
2016-05-24 13:04:45 +0300

    the end

--- tables/kolichestvo.php
+++ tables/kolichestvo.php
@@ -12,9 +12,12 @@ ON nomer.nom_komnaty=registration.nom_komnaty WHERE registr_id=0");
     }*/
     public function fetchKol(){
         $conn = $this->ConnectDB();
-        $stmt = $conn->prepare("SELECT client.passport, client.familia, client.imya, client.otchestvo,
-registration.data_zas, registration.data_vis, registration.data_vis-registration.data_zas AS kol_vo_dney
-FROM client INNER JOIN registration ON client.passport=registration.passport");
+        $stmt = $conn->prepare("SELECT registration.passport, client.familia, client.imya, client.otchestvo,
+registration.data_zas, registration.data_vis, registration.nom_komnaty, registration.data_vis-registration.data_zas
+AS kol_vo_sutok, cena_chel_sutki*( registration.data_vis-registration.data_zas) AS stoim_chel
+FROM ((class_nomera INNER JOIN nomer ON class_nomera.naimenov_id=nomer.naimenov_id)
+INNER JOIN registration ON nomer.nom_komnaty=registration.nom_komnaty)
+INNER JOIN client ON client.passport=registration.passport");
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
@@ -42,13 +45,15 @@ if (isset($_POST['menu'])){
         <nav id="header" class="navbar navbar-default navbar-fixed-top container" role="navigation">
             <table class="title table table-striped">
                 <tr>
-                    <td width="120">Серия и № паспорта</td>
-                    <td width="150">Фамилия</td>
-                    <td width="150">Имя</td>
-                    <td width="150">Отчество</td>
-                    <td width="120">Дата заселения</td>
-                    <td width="120">Дата выселения</td>
-                    <td width="150">Кол-во дней</td>
+                    <td width="130">Серия и № паспорта</td>
+                    <td width="140">Фамилия</td>
+                    <td width="120">Имя</td>
+                    <td width="140">Отчество</td>
+                    <td width="110">Дата заселения</td>
+                    <td width="110">Дата выселения</td>
+                    <td width="100">№ комнаты</td>
+                    <td width="100">Кол-во дней</td>
+                    <td width="120">Стоимость</td>
                 </tr>
             </table>
         </nav>
@@ -56,13 +61,15 @@ if (isset($_POST['menu'])){
             <table class="table table-striped"  style="text-align: center;">
                 <?php for ($i = 0; $i < count($rows); $i++) {?>
                     <tr>
-                        <td width="120"><?= $rows[$i]['passport']?></td>
-                        <td width="150"><?= $rows[$i]['familia']?></td>
-                        <td width="150"><?= $rows[$i]['imya']?></td>
-                        <td width="150"><?= $rows[$i]['otchestvo']?></td>
-                        <td width="120"><?= date('d.m.Y', strtotime($rows[$i]['data_zas']))?></td>
-                        <td width="120"><?= date('d.m.Y', strtotime($rows[$i]['data_vis']))?></td>
-                        <td width="150"><?= $rows[$i]['kol_vo_dney']?></td>
+                        <td width="130"><?= $rows[$i]['passport']?></td>
+                        <td width="140"><?= $rows[$i]['familia']?></td>
+                        <td width="120"><?= $rows[$i]['imya']?></td>
+                        <td width="140"><?= $rows[$i]['otchestvo']?></td>
+                        <td width="110"><?= date('d.m.Y', strtotime($rows[$i]['data_zas']))?></td>
+                        <td width="110"><?= date('d.m.Y', strtotime($rows[$i]['data_vis']))?></td>
+                        <td width="100"><?= $rows[$i]['nom_komnaty']?></td>
+                        <td width="100"><?= $rows[$i]['kol_vo_sutok']?></td>
+                        <td width="120"><?= $rows[$i]['stoim_chel']?></td>
                     </tr>
                 <?php } ?>
             </table>
